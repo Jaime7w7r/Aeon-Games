b090cb6cd658ed76de0ec9a66daf00daed2d88f9
Juanito <juanmamtzx202@gmail.com>
2022-11-30 16:42:27 -0600

    Fondo, logos_marcas, efecro clristal

--- header.php
+++ header.php
@@ -1,12 +1,11 @@
-
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 <style>
-    #carrito{
-    width:35px;
+#carrito {
+    width: 35px;
 }
 
-#add_icon{
+#add_icon {
     width: 20px;
     height: 20px;
     border-radius: 50px;
@@ -20,15 +19,16 @@
     justify-content: center;
 }
 
-#logo{
+#logo {
     width: 32px;
 }
 
-.login{
+.login {
     color: #eee;
     text-shadow: 0 0 10px #fff;
 }
-.slogan{
+
+.slogan {
     font-size: 14px;
     font-family: 'Syne Mono', monospace;
     color: white;
@@ -37,7 +37,7 @@
     text-shadow: 0 0 10px #fff;
 }
 
-.navbar{
+.navbar {
     background-color: rgba(0, 0, 0, 0.904);
     width: 100%;
     z-index: 100;
@@ -45,16 +45,17 @@
     height: 70px;
 }
 
-.abajo{
-    background-color: rgba(0, 0, 0, 0.726);
+.abajo {
+    background-color: rgba(0, 0, 0, 0.15);
+    backdrop-filter: blur(5px);
     margin-bottom: 70px;
 }
 
-.desparecer{
+.desparecer {
     top: -100px;
 }
 
-#aux_nav{
+#aux_nav {
     height: 70px;
     position: relative;
     background-color: rgba(95, 36, 36, 0.445);
@@ -65,41 +66,46 @@
 
 
 
-    <nav class="navbar navbar-expand-lg navbar-dark " id="nav">
-        <a class="navbar-brand" href="#"><img src="imagenes\logo.png" id="logo" alt=""></a>
-        <pre class="slogan">Hazlo facil 
+<nav class="navbar navbar-expand-lg navbar-dark " id="nav">
+    <a class="navbar-brand" href="#"><img src="imagenes\logo.png" id="logo" alt=""></a>
+    <pre class="slogan">Hazlo facil 
   Aeon Games</pre>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="<?php if ($_SERVER['PHP_SELF'] == "/curso/Proyecto_1/index.php"){ echo 'nav-item active'; }else{ echo 'nav-item'; } ?>">
-                    <a class="nav-link" href="index.php">Inicio</a>
-                </li>
-                <li class="<?php if ($_SERVER['PHP_SELF'] == "/curso/Proyecto_1/tienda.php"){ echo 'nav-item active'; }else{ echo 'nav-item'; } ?>">
-                    <a class="nav-link" href="tienda.php">Tienda</a>
-                </li>
-                <li class="<?php if ($_SERVER['PHP_SELF'] == "/curso/Proyecto_1/AcercaDe.php"){ echo 'nav-item active'; }else{ echo 'nav-item'; } ?>">
-                    <a class="nav-link" href="AcercaDe.php">Acerca De</a>
-                </li>
-                <li class="<?php if ($_SERVER['PHP_SELF'] == "/curso/Proyecto_1/contacto.php"){ echo 'nav-item active'; }else{ echo 'nav-item'; } ?>">
-                    <a class="nav-link " href="contacto.php">Contáctanos</a>
-                </li>
-                <li class="<?php if ($_SERVER['PHP_SELF'] == "/curso/Proyecto_1/ayuda.php"){ echo 'nav-item active'; }else{ echo 'nav-item'; } ?>">
-                    <a class="nav-link " href="ayuda.php">Ayuda</a>
-                </li>
-            </ul>
-            <button type="button" class="btn btn-outline-dark"><span class="login">Suscribete</span></button>
-            <button type="button" class="btn btn-outline-dark"><span class="login">Login</span></button>
-            <div>
-                <a href=""><img src="imagenes\carrito.png" alt="" id="carrito"></a>
-                <div id="add_icon"><span>1</span></div>
-            </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li
+                class="<?php if ($_SERVER['PHP_SELF'] == "/curso/Aeon-Games/index.php"){ echo 'nav-item active'; }else{ echo 'nav-item'; } ?>">
+                <a class="nav-link" href="index.php">Inicio</a>
+            </li>
+            <li
+                class="<?php if ($_SERVER['PHP_SELF'] == "/curso/Aeon-Games/tienda.php"){ echo 'nav-item active'; }else{ echo 'nav-item'; } ?>">
+                <a class="nav-link" href="tienda.php">Tienda</a>
+            </li>
+            <li
+                class="<?php if ($_SERVER['PHP_SELF'] == "/curso/Aeon-Games/AcercaDe.php"){ echo 'nav-item active'; }else{ echo 'nav-item'; } ?>">
+                <a class="nav-link" href="AcercaDe.php">Acerca De</a>
+            </li>
+            <li
+                class="<?php if ($_SERVER['PHP_SELF'] == "/curso/Aeon-Games/contacto.php"){ echo 'nav-item active'; }else{ echo 'nav-item'; } ?>">
+                <a class="nav-link " href="contacto.php">Contáctanos</a>
+            </li>
+            <li
+                class="<?php if ($_SERVER['PHP_SELF'] == "/curso/Aeon-Games/ayuda.php"){ echo 'nav-item active'; }else{ echo 'nav-item'; } ?>">
+                <a class="nav-link " href="ayuda.php">Ayuda</a>
+            </li>
+        </ul>
+        <button type="button" class="btn btn-outline-dark"><span class="login">Suscribete</span></button>
+        <button type="button" class="btn btn-outline-dark"><span class="login">Login</span></button>
+        <div>
+            <a href=""><img src="imagenes\carrito.png" alt="" id="carrito"></a>
+            <div id="add_icon"><span>1</span></div>
         </div>
-    </nav>
-    
-    <div id="aux_nav">
     </div>
+</nav>
+
+<div id="aux_nav">
+</div>

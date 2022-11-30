19454767e228546a2277de97027c2a68e5e1bb94
Jaime7w7r <jaimeadolfov07@gmail.com>
2022-11-29 23:24:27 -0600

    try

--- header.php
+++ header.php
@@ -68,7 +68,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark " id="nav">
         <a class="navbar-brand" href="#"><img src="imagenes\logo.png" id="logo" alt=""></a>
         <pre class="slogan">Hazlo facil 
-  AeonDev</pre>
+  Aeon Games</pre>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>

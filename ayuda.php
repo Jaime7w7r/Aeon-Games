4ea0d5cc91860e09a24c8510cc000da28ed63680
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2022-12-10 14:20:18 -0600

    agregado de diseño en ayuda y agregado de formulario pago OXXO en botones de pago 10/12/2022

--- ayuda.php
+++ ayuda.php
@@ -16,21 +16,35 @@
 <body>
     <?php include 'header.php'; ?>
     <div class="Hoja"><div class="preguntasF">
-        <h5>¿Cuánto tiempo tarda en llegar mi pedido?</h5>
-        <p>Los pedidos efectuados en la ciudad de Aguascalientes son enviados el mismo día que se realizan, siempre y cuando:
-            *El producto se encuentre fisicamente en alguna de la sucursales de Aguascalientes.
-            *La compra se haga en un horario entre 9:00 a.m. a 4:30 p.m.
-            En localidades fuera de Aguascalientes, así como otros en estados dentro de la República Mexicana, los tiempos de entrega rondan los 3 a 6 días hábiles aproximadamente.</p>
-        <h5>¿Con cuánto dinero puedo apartar una preventa?</h5>
-        <p>El monto puede variar, pero regularmente puedes apartar videojuegos (ediciones estándar), accesorios y coleccionables con $99 pesos. Si quieres apartar una consola en preventa el precio mínimo de apartado es de $500 pesos regularmente.</p>
-        <h5>No recuerdo la contraseña para entrar a mi cuenta, ¿Cómo puedo recuperarla?</h5>
-        <p>No te preocupes. A todos nos ha pasado. Da click en la siguiente liga ¿Olvido su NIP ó quiere recuperar su contraseña? y sigue las instrucciones.</p>
-        <h5>¿Cómo puedo solicitar una factura?</h5>
-        <p>Recuerda que debes solicitarla el mismo mes en el que se generó la compra. Da click en la siguiente liga y solicítala. (https://gameplanet.com/facturacion-electronica/)</p>
-        <h5>¿Cuánto me dan por mi juego?</h5>
-        <p>Acércate a tu tienda Gameplanet favorita. Ahí te podrán decir el monto que te abonaremos por tu juego. El precio de compra de tu juego es asignado de manera automática. Puedes llamar a nuestras sucursales vía telefónica para preguntar más detalles. Los números telefónicos los puedes encontrar en la sección de sucursales y horarios de este mismo sitio.</p>
-        <h5>¿Me reciben consolas o accesorios?</h5>
-        <p>Por el momento no pero no descartamos la posibilidad de hacerlo en el futuro. Mantente al pendiente de nuestro sitio para más información.</p>
+        <h2>Preguntas Frecuentes</h2>
+        <br>
+        <details open>
+            <summary> <h5>¿Cuánto tiempo tarda en llegar mi pedido?</h5></summary>
+            <p>Los pedidos efectuados en la ciudad de Aguascalientes son enviados el mismo día que se realizan, siempre y cuando:
+                *El producto se encuentre fisicamente en alguna de la sucursales de Aguascalientes.
+                *La compra se haga en un horario entre 9:00 a.m. a 4:30 p.m.
+                En localidades fuera de Aguascalientes, así como otros en estados dentro de la República Mexicana, los tiempos de entrega rondan los 3 a 6 días hábiles aproximadamente.</p>
+        </details>
+        <details>
+            <summary> <h5>¿Con cuánto dinero puedo apartar una preventa?</h5></summary>
+            <p>El monto puede variar, pero regularmente puedes apartar videojuegos (ediciones estándar), accesorios y coleccionables con $99 pesos. Si quieres apartar una consola en preventa el precio mínimo de apartado es de $500 pesos regularmente.</p>
+        </details>
+        <details>
+            <summary><h5>No recuerdo la contraseña para entrar a mi cuenta, ¿Cómo puedo recuperarla?</h5></summary>
+            <p>No te preocupes. A todos nos ha pasado. Da click en la siguiente liga ¿Olvido su NIP ó quiere recuperar su contraseña? y sigue las instrucciones.</p>
+        </details>
+        <details>
+            <summary><h5>¿Cómo puedo solicitar una factura?</h5></summary>
+            <p>Recuerda que debes solicitarla el mismo mes en el que se generó la compra. Da click en la siguiente liga y solicítala. (https://gameplanet.com/facturacion-electronica/)</p>
+        </details>
+        <details>
+            <summary><h5>¿Cuánto me dan por mi juego?</h5></summary>
+            <p>Acércate a tu tienda Gameplanet favorita. Ahí te podrán decir el monto que te abonaremos por tu juego. El precio de compra de tu juego es asignado de manera automática. Puedes llamar a nuestras sucursales vía telefónica para preguntar más detalles. Los números telefónicos los puedes encontrar en la sección de sucursales y horarios de este mismo sitio.</p>
+        </details>
+        <details>
+            <summary><h5>¿Me reciben consolas o accesorios?</h5></summary>
+            <p>Por el momento no pero no descartamos la posibilidad de hacerlo en el futuro. Mantente al pendiente de nuestro sitio para más información.</p>
+        </details>
     </div>
     
     <div class="Formulario"><form action="ayuda.php" method="post">
